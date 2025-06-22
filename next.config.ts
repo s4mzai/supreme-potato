0100663e37dcff4b741adcdfe9e347bba3734ad9
s4mzai <hi.samar.7x@gmail.com>
2025-06-22 22:35:36 +0530

    somechanges

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    domains:['images.unsplash.com',"lh3.googleusercontent.com","avatars.githubusercontent.com"]
+  images: {
+    domains: ['images.unsplash.com', "lh3.googleusercontent.com", "avatars.githubusercontent.com"]
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  }
+  // Move from experimental to root level
+  serverExternalPackages: ['@prisma/client', 'prisma'],
 };
 
 export default nextConfig;
